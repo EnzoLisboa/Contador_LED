641a18cfaa301584a411668fbdc9ad539a8d4107
Enzo Lisboa <lisboaenzo@gmail.com>
2025-01-27 18:01:11 -0300

    Primeiro commit

---
+++ tarefa.embarca.c
@@ -0,0 +1,311 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/bootrom.h"
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "matriz_leds.h"
+#include "pico/multicore.h"
+
+// Arquivo .pio para controle da matriz
+#include "pio_matrix.pio.h"
+
+// Pino que realizará a comunicação do microcontrolador com a matriz
+#define OUT_PIN 7
+
+// Gera o binário que controla a cor de cada célula do LED
+// rotina para definição da intensidade de cores do led
+uint32_t gerar_binario_cor(double red, double green, double blue)
+{
+  unsigned char RED, GREEN, BLUE;
+  RED = red * 255.0;
+  GREEN = green * 255.0;
+  BLUE = blue * 255.0;
+  return (GREEN << 24) | (RED << 16) | (BLUE << 8);
+}
+
+uint configurar_matriz(PIO pio){
+    bool ok;
+
+    // Define o clock para 128 MHz, facilitando a divisão pelo clock
+    ok = set_sys_clock_khz(128000, false);
+
+    // Inicializa todos os códigos stdio padrão que estão ligados ao binário.
+    stdio_init_all();
+
+    printf("iniciando a transmissão PIO");
+    if (ok) printf("clock set to %ld\n", clock_get_hz(clk_sys));
+
+    //configurações da PIO
+    uint offset = pio_add_program(pio, &pio_matrix_program);
+    uint sm = pio_claim_unused_sm(pio, true);
+    pio_matrix_program_init(pio, sm, offset, OUT_PIN);
+
+    return sm;
+}
+
+void imprimir_desenho(Matriz_leds_config configuracao, PIO pio, uint sm){
+    for (int contadorLinha = 4; contadorLinha >= 0; contadorLinha--){
+        if(contadorLinha % 2){
+            for (int contadorColuna = 0; contadorColuna < 5; contadorColuna ++){
+                uint32_t valor_cor_binario = gerar_binario_cor(
+                    configuracao[contadorLinha][contadorColuna].red,
+                    configuracao[contadorLinha][contadorColuna].green,
+                    configuracao[contadorLinha][contadorColuna].blue
+                );
+
+                pio_sm_put_blocking(pio, sm, valor_cor_binario);
+            }
+        }else{
+            for (int contadorColuna = 4; contadorColuna >= 0; contadorColuna --){
+                uint32_t valor_cor_binario = gerar_binario_cor(
+                    configuracao[contadorLinha][contadorColuna].red,
+                    configuracao[contadorLinha][contadorColuna].green,
+                    configuracao[contadorLinha][contadorColuna].blue
+                );
+
+                pio_sm_put_blocking(pio, sm, valor_cor_binario);
+            }
+        }
+    }
+}
+
+//Pino do led vermelho
+#define LED_VERMELHO_PIN 11
+
+//Pinos para leitura dos botões
+#define BUTTON_A_PIN 5
+#define BUTTON_B_PIN 6
+
+//Função para piscar o led
+void piscar_led()
+{
+    //Inicializando o led vermelho
+    gpio_init(LED_VERMELHO_PIN);
+    gpio_set_dir(LED_VERMELHO_PIN, GPIO_OUT);
+    // Liga o LED
+    gpio_put(LED_VERMELHO_PIN, 1);
+    sleep_ms(100); // Espera 100 milissegundos
+    // Desliga o LED
+    gpio_put(LED_VERMELHO_PIN, 0);
+    sleep_ms(100); // Espera 100 milissegundos
+}
+
+
+void n_zero(PIO pio, uint sm)
+{
+    Matriz_leds_config matriz_zero = {
+        //   Coluna 0         Coluna 1         Coluna 2         Coluna 3         Coluna 4
+        // R    G    B      R    G    B      R    G    B      R    G    B      R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 4
+    };
+    
+    imprimir_desenho(matriz_zero, pio, sm);
+}
+
+void n_um(PIO pio, uint sm)
+{
+    Matriz_leds_config matriz_um = {
+        //   Coluna 0         Coluna 1         Coluna 2         Coluna 3         Coluna 4
+        // R    G    B      R    G    B      R    G    B      R    G    B      R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 4
+    };
+    
+    imprimir_desenho(matriz_um, pio, sm);
+}
+
+void n_dois(PIO pio, uint sm)
+{
+    Matriz_leds_config matriz_dois = {
+        //   Coluna 0         Coluna 1         Coluna 2         Coluna 3         Coluna 4
+        // R    G    B      R    G    B      R    G    B      R    G    B      R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 4
+    };
+    
+    imprimir_desenho(matriz_dois, pio, sm);
+}
+
+void n_tres(PIO pio, uint sm)
+{
+    Matriz_leds_config matriz_tres = {
+        //   Coluna 0         Coluna 1         Coluna 2         Coluna 3         Coluna 4
+        // R    G    B      R    G    B      R    G    B      R    G    B      R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 4
+    };
+    
+    imprimir_desenho(matriz_tres, pio, sm);
+}
+
+void n_quatro(PIO pio, uint sm)
+{
+    Matriz_leds_config matriz_quatro = {
+        //   Coluna 0         Coluna 1         Coluna 2         Coluna 3         Coluna 4
+        // R    G    B      R    G    B      R    G    B      R    G    B      R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 4
+    };
+    
+    imprimir_desenho(matriz_quatro, pio, sm);
+}
+
+void n_cinco(PIO pio, uint sm)
+{
+    Matriz_leds_config matriz_cinco = {
+        //   Coluna 0         Coluna 1         Coluna 2         Coluna 3         Coluna 4
+        // R    G    B      R    G    B      R    G    B      R    G    B      R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 4
+    };
+    imprimir_desenho(matriz_cinco, pio, sm);
+}
+
+void n_seis(PIO pio, uint sm)
+{
+    Matriz_leds_config matriz_seis = {
+        //   Coluna 0         Coluna 1         Coluna 2         Coluna 3         Coluna 4
+        // R    G    B      R    G    B      R    G    B      R    G    B      R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 4
+    };
+    
+    imprimir_desenho(matriz_seis, pio, sm);
+}
+
+void n_sete(PIO pio, uint sm)
+{
+    Matriz_leds_config matriz_sete = {
+        //   Coluna 0         Coluna 1         Coluna 2         Coluna 3         Coluna 4
+        // R    G    B      R    G    B      R    G    B      R    G    B      R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 4
+    };
+    
+    imprimir_desenho(matriz_sete, pio, sm);
+}
+
+void n_oito(PIO pio, uint sm)
+{
+    Matriz_leds_config matriz_oito = {
+        //   Coluna 0         Coluna 1         Coluna 2         Coluna 3         Coluna 4
+        // R    G    B      R    G    B      R    G    B      R    G    B      R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 4
+    };
+    
+    imprimir_desenho(matriz_oito, pio, sm);
+}
+
+void n_nove(PIO pio, uint sm)
+{
+    Matriz_leds_config matriz_nove = {
+        //   Coluna 0         Coluna 1         Coluna 2         Coluna 3         Coluna 4
+        // R    G    B      R    G    B      R    G    B      R    G    B      R    G    B
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 0
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 1
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 2
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 3
+        {{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}}, // Linha 4
+    };
+    
+    imprimir_desenho(matriz_nove, pio, sm);
+}
+
+int main()
+{
+    stdio_init_all();
+
+    //Inicializando os botões
+    gpio_init(BUTTON_A_PIN);
+    gpio_set_dir(BUTTON_A_PIN, GPIO_IN);
+    gpio_pull_down(BUTTON_A_PIN);
+
+    gpio_init(BUTTON_B_PIN);
+    gpio_set_dir(BUTTON_B_PIN, GPIO_IN);
+    gpio_pull_down(BUTTON_B_PIN);
+
+    int numero = 0;
+    PIO pio = pio0;
+    uint sm = configurar_matriz(pio);
+    
+    while (true)
+    {   
+        piscar_led();
+        // Verificando os botões
+        if (gpio_get(BUTTON_A_PIN)) {
+            numero++;
+            sleep_ms(200);
+            if (numero > 9) {numero = 9;} //Limitando o numero
+        }
+
+        if (gpio_get(BUTTON_B_PIN)) {
+            numero--;
+            sleep_ms(200);
+             if (numero < 0) {numero = 0;} //Limitando o numero
+        }
+
+        // Chamando as funções de acordo com o valor de numero
+        switch(numero) {
+            case 1:
+                n_um(pio, sm);
+                break;
+            case 2:
+                n_dois(pio, sm);
+                break;
+            case 3:
+                n_tres(pio, sm);
+                break;
+            case 4:
+                n_quatro(pio, sm);
+                break;
+            case 5:
+                n_cinco(pio, sm);
+                break;
+            case 6:
+                n_seis(pio, sm);
+                break;
+            case 7:
+                n_sete(pio, sm);
+                break;
+            case 8:
+                n_oito(pio, sm);
+                break;
+            case 9:
+                n_nove(pio, sm);
+                break;
+            case 0:
+                n_zero(pio, sm);
+                break;
+        }  
+    } 
+}
